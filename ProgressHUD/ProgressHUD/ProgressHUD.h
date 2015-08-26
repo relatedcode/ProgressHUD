895e9c5e7ff5218b7ca171af752e016017227051
Jonathan Koch <jbkoch@mac.com>
2015-08-26 12:10:11 +0200

    change border colour from orange to white

--- ProgressHUD/ProgressHUD/ProgressHUD.h
+++ ProgressHUD/ProgressHUD/ProgressHUD.h
@@ -15,7 +15,7 @@
 #define HUD_STATUS_FONT			[UIFont boldSystemFontOfSize:16]
 #define HUD_STATUS_COLOR		[UIColor whiteColor]
 
-#define HUD_SPINNER_COLOR		[UIColor colorWithRed:255.0f/255.0f green:106.0f/255.0f blue:56.0f/255.0f alpha:1.0f]
+#define HUD_SPINNER_COLOR		[UIColor colorWithRed:240.0f/255.0f green:240.0f/255.0f blue:240.0f/255.0f alpha:1.0f]
 #define HUD_BACKGROUND_COLOR	[UIColor colorWithWhite:0.0f alpha:0.8f]
 #define HUD_WINDOW_COLOR		[UIColor colorWithRed:0.0f green:0.0f blue:0.0f alpha:0.2f]
 
